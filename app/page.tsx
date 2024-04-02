2c284bc3104f790229a6e8e62cbd9c69a648d94b
Enzo <enzzzooo@proton.me>
2024-04-02 22:27:27 +0000

    bg and home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default async function page() {
   return (
-    <div className="flex flex-col items-center space-y-40 text-7xl ">
+    <>
+    <div className=" z-[-1] absolute w-screen h-screen bg-cover blur-sm bg-[url('../public/background-forest.jpeg')]"></div>
+    <div className="flex flex-col items-center space-y-40 text-9xl [&>*]:hover:blur-sm ">
       <h1>Enzzzooo's Portfolio</h1>
       <p>A portfolio of my projects and website designs</p>
     </div>
+    </>
   )
 }

3ae1ffb28e4a4842c052dafa1b707b59e5413203
enzzzooo <enzzzooo@proton.me>
2024-04-03 00:27:23 -0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@ export default async function page() {
   return (
     <>
     <div className=" z-[-1] absolute w-screen h-screen bg-cover blur-sm bg-[url('../public/background-forest.jpeg')]"></div>
-    <div className="flex flex-col mx-5 items-center space-y-40 text-9xl">
-      <h1 className="hover:blur-sm">Enzzzooo's Portfolio</h1>
-      <p className="hover:blur-sm">A portfolio of my projects and website designs</p>
-    </div>
+    <div className=" mx-5 space-y-40 text-9xl hover:*:blur-sm *:inline-block *:&nbsp;">
+      {/* hover:*:blur-sm applys blur-sm to children, not the parent div, and for each child individually, nice */}
+        <h1>Enzzzooo's</h1>
+        <h1>Portfolio</h1><br />
+        <p>A</p>
+        <p>portfolio</p>
+        <p>of</p>
+        <p>my</p>
+        <p>projects</p>
+        <p>and</p>
+        <p>website</p>
+        <p>designs</p>
+      </div>
     </>
   )
 }
--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -14,31 +14,22 @@ export default function Menu(){
 // by default false
     const [menu, setMenu] = useState(false);
 
-    // when opened, true
-    const toggleMenu = () => {
-        // opposite of current state, when clicked, which triggers this function
-        setMenu(!menu);
-    }
-    // sets false to close
-    const closeMenu = () => {
-        setMenu(false);
-    }
     return (
         <div className={`flex justify-between mx-5 bg-grn ${menu && "absolute w-screen h-screen"}`}>
             {/* logo */}
-            <Image alt="Logo" src={logo} onClick={closeMenu} className='size-20'></Image>
+            <Image alt="Logo" src={logo} onClick={() => {if (!menu) {setMenu(false)}}} className='size-20'></Image>
             {/* links */}
             <ul className={menu == true ? "block" : "hidden"}>
                 {menuLinks.map((link, index) => (
                     <li key={index}>
-                        <Link href={link.path} onClick={closeMenu}>
+                        <Link href={link.path} onClick={() => setMenu(!menu)}>
                             {link.label}
                         </Link>
                     </li>
                 ))}
             </ul>
             {/* menu open/close */}
-            <button onClick={toggleMenu}>
+            <button onClick={() => setMenu(!menu)}>
                 {/* if menu == true (open) then X, else Menu */}
                 {menu ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12`" /></svg> : "Menu"}

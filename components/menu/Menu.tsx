79cbacd28206ec1cbbf4543927f2b2309a100aa4
enzzzooo <enzzzooo@proton.me>
2024-04-04 15:47:14 -0500

    need to specificy size or else X will center

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import logo from "../../public/logo.jpeg";
 import Link from "next/link";
 import ScrollLock from "./ScrollLock";
-
+// similar to https://www.nusailing.com/#nust
 export default function Menu(){
     const menuLinks = [
         {path: "/", label: "Home"},
@@ -15,20 +15,20 @@ export default function Menu(){
     ]
 // by default false
     const [menu, setMenu] = useState(false);
-    const [blockScroll, allowScroll] = ScrollLock();
-    if (menu) {
-        blockScroll();
-    }
-    if (!menu) {
-        allowScroll();
-    }
+    // const [blockScroll, allowScroll] = ScrollLock();
+    // if (menu) {
+    //     blockScroll();
+    // }
+    // if (!menu) {
+    //     allowScroll();
+    // }
     return (
-        <div className={`flex justify-between px-5 bg-green ${menu && "fixed w-screen h-screen"}`}>
+        <div className={`flex justify-between px-5 bg-green ${menu && "relative w-screen h-screen overflow-hidden"}`}>
             {/* logo */}
             <Link href="/"><Image alt="Logo" src={logo} onClick={() => {if (!menu) {setMenu(false)}}} className='size-20'></Image></Link>
             {/* links */}
             {/* if there is no menu open then display hidden */}
-            <ul className={`!menu && "hidden"`}>
+            <ul className={!menu ? "hidden" : ""}>
                 {menuLinks.map((link, index) => (
                     <li key={index}>
                         <Link href={link.path} onClick={() => setMenu(!menu)}>
@@ -38,7 +38,8 @@ export default function Menu(){
                 ))}
             </ul>
             {/* menu open/close */}
-            <button onClick={() => setMenu(!menu)}>
+            {/* need to specificy size or else X will go to center */}
+            <button className="size-20" onClick={() => setMenu(!menu)}>
                 {/* if menu == true (open) then X, else Menu */}
                 {/* the X is unicode number 10,005 https://www.fileformat.info/info/unicode/char/2715/index.htm by react https://shripadk.github.io/react/docs/jsx-gotchas.html */}
                 {menu ? String.fromCharCode(10005) : "Menu"}
